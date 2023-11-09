6c661596876d83162fc81b5f0fcf5a150893aae0
zbuchheit <zbuchheit@pulumi.com>
2023-11-08 17:05:05 -0800

    add session 2 solution

--- Program.cs
+++ Program.cs
@@ -10,30 +10,34 @@ return await Pulumi.Deployment.RunAsync(() =>
     // TODO: 5. Generate a new password
     var password = new RandomPassword("password", new RandomPasswordArgs
     {
-                                                                                    // FIXME
+        Length = 20,
+        Special = true,
     });
 
     // TODO: 6. Generate a random pet name
-    var petName = new                                                               // FIXME
+    var petName = new RandomPet("stack-pet", new RandomPetArgs
+    {
+
+    });
 
     // TODO: 7. Concatenate the `stackOwner` value with the random pet name (`ownerName-petName`)
-    var ownerName = config.Require("");                                             // FIXME
-    var ownerPetName = $"{petName.Id}, {ownerName}";                                // FIXME
-    Console.WriteLine($"Owner pet name: {n}!");                                     // FIXME
+    var ownerName = config.Require("stackOwner");
+    var ownerPetName = Output.Format($"{petName.Id}, {ownerName}");
+    ownerPetName.Apply(n => { Console.WriteLine($"Owner pet name: {n}!"); return n; });
 
     // TODO: 8. Use pulumi.all() to create a welcome message "`Hello dear <ownerName>, this is your pet <petName>.`"
-    var welcomeMessage = $"Hello dear {ownerName}, this is your pet {petName}" );   // FIXME
+    var welcomeMessage = Output.All(Output.Create(ownerName), petName.Id).Apply(t => $"Hello dear {t[0]}, this is your pet {t[1]}" );
 
     // TODO: 9. Create stack outputs (See README.md)
     return new Dictionary<string, object?>
     {
-        ["password"] = ,                                                            // FIXME
-        ["stackPet"] = ,                                                            // FIXME
-        ["ownerPetName"] = ,                                                        // FIXME
-        ["apiKey"] = ,                                                              // FIXME
-        ["insecureApiKey"] = ,                                                      // FIXME
-        ["welcomeMessage"] = ,                                                      // FIXME
-        // TODO: 10. Set a stack README for your stack
-        ["readme"] = ,                                                              // FIXME
+        ["password"] = password.Result,
+        ["stackPet"] = petName.Id,
+        ["ownerPetName"] = ownerPetName,
+        ["apiKey"] = config.RequireSecret("apiKey"),
+        ["insecureApiKey"] = Output.Unsecret(config.RequireSecret("apiKey")),
+        ["welcomeMessage"] = welcomeMessage,
+        // TODO: 11. Set a stack README for your stack
+        ["readme"] = System.IO.File.ReadAllText("./Pulumi.README.md"),
     };
 });
