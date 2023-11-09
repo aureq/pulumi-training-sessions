a72fa8a576daf89ba19af86c9dcbce14782585f5
zbuchheit <zbuchheit@pulumi.com>
2023-11-09 09:16:32 -0800

    add session 3 solution

--- Program.cs
+++ Program.cs
@@ -17,64 +17,176 @@ return await Pulumi.Deployment.RunAsync(() =>
 
     var username = config.Require("username");
     // TODO 3: Generate a random password
-    var password = new RandomPassword                                       // FIXME
+    var password = new RandomPassword("password", new RandomPasswordArgs
+    {
+        Length = 33,
+        Special = false,
+    });
 
     var resourceGroup = new ResourceGroup("resourceGroup");
 
     // TODO 5: 3rd pary module to compute subnets CIDR
-    IPNetwork ipnetwork = IPNetwork.Parse(/* FIXME */);                     // FIXME
+    IPNetwork ipnetwork = IPNetwork.Parse(cidrBlock);
 
     // TODO 2: Virtual Network
-    var network = new VirtualNetwork("network", new VirtualNetworkArgs      // FIXME
+    var network = new VirtualNetwork("network", new VirtualNetworkArgs
     {
-        /* FIXME */
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
+        AddressSpace = new AddressSpaceArgs
+        {
+            AddressPrefixes = { ipnetwork.ToString() },
+        },
     }, new CustomResourceOptions
     {
         // TODO 2: Nest resource under correct parent
-        /* FIXME */
+        Parent = resourceGroup
     });
 
     // TODO 2: public Subnet (/20)
     // TODO 5: 3rd pary module to compute subnets CIDR
-    var subnetBytes = IPNetwork.ToCidr( /* FIXME */);                       // FIXME
+    var subnetBytes = IPNetwork.ToCidr(IPAddress.Parse(subnetMask));
     IPNetworkCollection subnets = ipnetwork.Subnet(subnetBytes);
-    var subnet = new Subnet("subnet",                                       // FIXME
-        /* FIXME */
-    );
+    var subnet = new Subnet("subnet", new Pulumi.AzureNative.Network.SubnetArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        VirtualNetworkName = network.Name,
+        AddressPrefix = subnets[0].ToString(),
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = network
+    });
 
     // TODO 2: public IP address
-    var publicIp = new PublicIPAddress("public-ip",                         // FIXME
+    var publicIp = new PublicIPAddress("public-ip", new Pulumi.AzureNative.Network.PublicIPAddressArgs
     {
-        /* FIXME */
+        Location = resourceGroup.Location,
+        ResourceGroupName = resourceGroup.Name,
+        PublicIPAllocationMethod = IPAllocationMethod.Static,
+        Sku = new Pulumi.AzureNative.Network.Inputs.PublicIPAddressSkuArgs
+        {
+            Tier = Pulumi.AzureNative.Network.PublicIPAddressSkuTier.Regional,
+            Name = "Standard"
+        },
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = network
     });
 
     // TODO 2: network security group
-    var networkSG = new NetworkSecurityGroup("network-sg",                  // FIXME
+    var networkSG = new NetworkSecurityGroup("network-sg", new Pulumi.AzureNative.Network.NetworkSecurityGroupArgs
     {
-        /* FIXME */
+        ResourceGroupName = resourceGroup.Name,
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = resourceGroup
     });
 
     // TODO 2: network security rule
-    var sgRule = new SecurityRule("rule",                                   // FIXME
+    var sgRule = new SecurityRule("rule", new Pulumi.AzureNative.Network.SecurityRuleArgs
     {
-        /* FIXME */
+        ResourceGroupName = resourceGroup.Name,
+        NetworkSecurityGroupName = networkSG.Name,
+        Access = "Allow",
+        Direction = "Inbound",
+        Priority = 100,
+        Protocol = "Tcp",
+        DestinationPortRange = "22",
+        SourcePortRange = "*",
+        SourceAddressPrefix = "*",
+        DestinationAddressPrefix = "*"
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = networkSG
     });
 
     // TODO 2: network interface
-    var networkInterface = new NetworkInterface("vm-nic",                   // FIXME
+    var networkInterface = new NetworkInterface("vm-nic", new NetworkInterfaceArgs
     {
-        /* FIXME */
+        ResourceGroupName = resourceGroup.Name,
+        IpConfigurations =
+        {
+            new NetworkInterfaceIPConfigurationArgs
+            {
+                Name = "vmserveripcfg",
+                Subnet = new Pulumi.AzureNative.Network.Inputs.SubnetArgs
+                {
+                    Id = subnet.Id
+                },
+                PrivateIPAllocationMethod = IPAllocationMethod.Dynamic,
+                PublicIPAddress = new Pulumi.AzureNative.Network.Inputs.PublicIPAddressArgs
+                {
+                    Id = publicIp.Id,
+                }
+            }
+        },
+        NetworkSecurityGroup = new Pulumi.AzureNative.Network.Inputs.NetworkSecurityGroupArgs
+        {
+            Id = networkSG.Id,
+        },
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = subnet
     });
 
     // TODO 3: Create a virtual machine
-    var virtualMachine = new VirtualMachine("vm",                           // FIXME
+    var virtualMachine = new VirtualMachine("vm", new VirtualMachineArgs
     {
-        /* FIXME */
+        ResourceGroupName = resourceGroup.Name,
+        NetworkProfile = new Pulumi.AzureNative.Compute.Inputs.NetworkProfileArgs
+        {
+            NetworkInterfaces =
+            {
+                new NetworkInterfaceReferenceArgs
+                {
+                    Id = networkInterface.Id,
+                    Primary = true,
+                }
+            }
+        },
+        HardwareProfile = new HardwareProfileArgs
+        {
+            VmSize = VirtualMachineSizeTypes.Standard_A0,
+        },
+        StorageProfile = new StorageProfileArgs
+        {
+            OsDisk = new OSDiskArgs
+            {
+                CreateOption = DiskCreateOptionTypes.FromImage,
+                Name = "osdisk",
+            },
+            ImageReference = new ImageReferenceArgs
+            {
+                // TODO 3: Using the most recent Ubuntu 20.04 LTS
+                Publisher = "Canonical",
+                Offer = "0001-com-ubuntu-server-focal",
+                Sku = "20_04-lts",
+                Version = "latest",
+            }
+        },
+        OsProfile = new OSProfileArgs
+        {
+            ComputerName = "hostname",
+            // TODO 3: Use random password
+            AdminPassword = password.Result,
+            AdminUsername = username,
+        },
+    }, new CustomResourceOptions
+    {
+        // TODO 2: Nest resource under correct parent
+        Parent = networkInterface
     });
 
     // TODO 4: Create stack outputs
-    return new Dictionary<string, object?>                                  // FIXME
+    return new Dictionary<string, object?>
     {
-        /* FIXME */
+        ["username"] = username,
+        ["password"] = password.Result,
+        ["ip"] = publicIp.IpAddress,
     };
 });
--- README.md
+++ README.md
@@ -1,39 +1,16 @@
 # Pulumi training using C# (3rd session)
 
-Exercise to learn how to use Pulumi (3rd session)
-
-## Introduction
-
-In this session, you will learn how to create a fully functional VirtualNetwork and a virtual machine. You'll also learn how to use 3rd party library to perform tasks more efficiently and in a reliable way. You'll have another opportunity to use string concatenation on `Output<T>`.
-
-## Content
-
-1. Create a new stack and install the node modules (`dotnet restore`)
-2. Create the following resources, and make sure they are all nested under the correct parent
-   * A Virtual Network (10.42.0.0/16)
-   * A public Subnet (/20)
-   * A public IP address
-   * A network interface
-   * The necessarity network security group and rule
-3. Create a virtual machine
-   * Use a small virtual machine to limit/reduce cost
-   * Using the most recent Ubuntu 20.04 LTS
-   * Generate a random password and use it
-   * ensure you can SSH into it
-4. Create stack outputs for:
-   * The VM host name or IP address
-   * The random password
-   * A configuration admin user name for the VM
-
-## Bonus/Challenges
-
-5. Use a 3rd pary module to compute subnets CIDR
-6. Ensure the project can easily be configured (ie, no hardcoded values where possible)
-
-## Resources
-
-* Pulumi [examples](https://github.com/pulumi/examples)
+Answers to the 3rd training session
 
 ## Answers
 
-You will find all the answers on the `azure-cs/session-3-solution` branch.
+1. `pulumi stack init loginName/dev`
+2. See [Program.cs](Program.cs)
+3. See [Program.cs](Program.cs)
+4. See [Program.cs](Program.cs)
+5. `dotnet add package ipnetwork2` and the references to `IPNetwork` in [Program.cs](Program.cs)
+6. In [Program.cs](Program.cs)
+   * all references to `Pulumi.Config()` and `config.Require()`
+   * `pulumi config set username aurelien`
+   * `pulumi config set cidrBlock 10.42.0.0/16`
+   * `pulumi config set netmask 255.255.240.0`
