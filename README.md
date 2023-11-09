e3ee6cf729457e728bd05b66617da72d8b1a8a31
zbuchheit <zbuchheit@pulumi.com>
2023-11-09 10:38:38 -0800

    add session 3 solution

--- README.md
+++ README.md
@@ -1,39 +1,15 @@
 # Pulumi training using TypeScript (3rd session)
 
-Exercises to learn how to use Pulumi (3rd session)
-
-## Introduction
-
-In this session, you will learn how to create a fully functional VirtualNetwork and a virtual machine. You'll also learn how to use 3rd party library to perform tasks more efficiently and in a reliable way. You'll have another opportunity to use string concatenation on `Output<T>`.
-
-## Content
-
-1. Create a new stack and install the node modules (`npm install`)
-2. Create the following resources, and make sure they are all nested under the correct parent
-   * A Virtual Network (10.42.0.0/16)
-   * A public Subnet (/20)
-   * A public IP address
-   * A network interface
-   * The necessarity network security group and rule
-3. Create a virtual machine
-   * Use a small virtual machine to limit/reduce cost
-   * Using the most recent Ubuntu 20.04 LTS
-   * Generate a random password
-   * ensure you can SSH into it
-4. Create stack outputs for:
-   * The VM host name or IP address
-   * The random password
-   * A configuration admin user name for the VM
-
-## Bonus/Challenges
-
-5. Use a 3rd pary module to compute subnets CIDR
-6. Ensure the project can easily be configured (ie, no hardcoded values where possible)
-
-## Resources
-
-* Pulumi [examples](https://github.com/pulumi/examples)
+Answers to the 3rd training 3rd session
 
 ## Answers
 
-You will find all the answers on the aws-ts/session-3-solution branch.
+1. `pulumi stack init loginName/dev`
+2. See [index.ts](index.ts)
+3. See [index.ts](index.ts)
+4. See [index.ts](index.ts)
+5. `npm install "@types/netmask" netmask` and the references to `Netmask` in [index.ts](index.ts)
+6. In [index.ts](index.ts)
+   * all references to `pulumi.Config()` and `config.require()`
+   * `pulumi config set cidrBlock 10.42.0.0/16`
+   * `pulumi config set netmask 255.255.240.0`
--- index.ts
+++ index.ts
@@ -10,8 +10,10 @@ export = async () => {
     // Create an Azure Resource Group
     const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
 
-    const cidrBlock = /* */;
-    const subnetMask = /* */;
+    const cidrBlock = config.require("cidrBlock");
+    const subnetMask = config.require("netmask");
+    const username = config.require("username");
+
 
     const virtualNetworkCidr = new Netmask(cidrBlock.toString());
     const subnetCidr = new Netmask(`${virtualNetworkCidr.base}/${subnetMask}`);
@@ -19,42 +21,78 @@ export = async () => {
     const virtualNetwork = new azure.network.VirtualNetwork("network", {
         resourceGroupName: resourceGroup.name,
         location: resourceGroup.location,
-        addressSpaces: [ /* */ ],
+        addressSpaces: [cidrBlock],
     });
 
     const subnet = new azure.network.Subnet("subnet", {
         resourceGroupName: resourceGroup.name,
-        virtualNetworkName: /* */,
-        addressPrefixes: /* */,
+        virtualNetworkName: virtualNetwork.name,
+        addressPrefixes: [`${subnetCidr.base}/${subnetCidr.bitmask}`],
     });
 
     const pubIp = new azure.network.PublicIp("ip", {
-        /* */
+        location: resourceGroup.location,
+        resourceGroupName: resourceGroup.name,
+        allocationMethod: "Static",
+        sku: "Standard",
     });
 
     const nic = new azure.network.NetworkInterface("nic", {
-        /* */
+        resourceGroupName: resourceGroup.name,
+        ipConfigurations: [{
+            name: "webserveripcfg",
+            subnetId: subnet.id,
+            privateIpAddressAllocation: "Dynamic",
+            publicIpAddressId: pubIp.id,
+        }],
     });
 
     const sg = new azure.network.NetworkSecurityGroup("sg", {
-        /* */
+        resourceGroupName: resourceGroup.name,
+        location: resourceGroup.location,
     });
 
     const sgrule = new azure.network.NetworkSecurityRule("sgrule", {
-        /* */
+        priority: 100,
+        direction: "Inbound",
+        access: "Allow",
+        protocol: "Tcp",
+        sourcePortRange: "*",
+        destinationPortRange: "22",
+        sourceAddressPrefix: "*",
+        destinationAddressPrefix: "*",
+        resourceGroupName: resourceGroup.name,
+        networkSecurityGroupName: sg.name,
     });
 
     const sgnicassoc = new azure.network.NetworkInterfaceSecurityGroupAssociation("sg-nic-assoc", {
-        /* */
+        networkInterfaceId: nic.id,
+        networkSecurityGroupId: sg.id,
     });
 
-
     const password = new random.RandomPassword('password', {
-        /* */
-    })./* */;
+        length: 33,
+        special: false,
+    }).result;
 
     const vm = new azure.compute.VirtualMachine("vm", {
-        /* */
+        resourceGroupName: resourceGroup.name,
+        networkInterfaceIds: [nic.id],
+        vmSize: "Standard_A0",
+        deleteDataDisksOnTermination: true,
+        deleteOsDiskOnTermination: true,
+        osProfile: {
+            computerName: "hostname",
+            adminUsername: username,
+            adminPassword: password,
+        },
+        osProfileLinuxConfig: {
+            disablePasswordAuthentication: false,
+        },
+        storageOsDisk: {
+            createOption: "FromImage",
+            name: "osdisk1",
+        },
         storageImageReference: {
             publisher: "Canonical",
             offer: "0001-com-ubuntu-server-focal",
@@ -64,7 +102,9 @@ export = async () => {
     });
 
     return {
-        /* */
+        username: username,
+        password: password,
+        ip: pubIp.ipAddress,
     }
 
 }
