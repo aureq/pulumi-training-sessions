9c87fe617621bd8a309b383e5e280750779c93ac
zbuchheit <zbuchheit@pulumi.com>
2023-11-09 10:33:25 -0800

    add session 2 solution

--- index.ts
+++ index.ts
@@ -7,29 +7,32 @@ const config = new pulumi.Config();
 export = async () => {
   // TODO: 5. Generate a new password
   const password = new random.RandomPassword("password", {
-                                                        /* FIXME */
+    length: 20,
+    special: true,
   });
 
   // TODO: 6. Generate a random pet name
-  const petName =                                       /* FIXME */
+  const petName = new random.RandomPet("stack-pet");
 
-  // TODO: 7. Concatenate the `stackOwner` value with the random pet name (format: `owner-pet`)
-  const ownerName = config.require("");                 /* FIXME */
-  const ownerPetName = `${ownerName}-${petName.id}`;    /* FIXME */
-  console.log("Owner pet name: ", ownerPetName);        /* FIXME */
+  // TODO: 7. Concatenate the `stackOwner` value with the random pet name (`ownerName-petName`)
+  const ownerName = config.require("stackOwner");
+  const ownerPetName = pulumi.interpolate`${ownerName}-${petName.id}`;
+  ownerPetName.apply((n) => console.log("Owner pet name: ", n));
 
   // TODO: 8. Use pulumi.all() to create a welcome message "`Hello dear <ownerName>, this is your pet <petName>.`"
-  const welcomeMessage = `Hello dear ${o}, this is your pet ${p}.`;
+  const welcomeMessage = pulumi.all([ownerName, petName.id]).apply(([o, p]) => {
+    return `Hello dear ${o}, this is your pet ${p}.`;
+  });
 
   // TODO: 9. Create stack outputs (See README.md)
   return {
-    password: ,                                         /* FIXME */
-    stackPet: ,                                         /* FIXME */
-    ownerPetName: ,                                     /* FIXME */
-    apiKey: ,                                           /* FIXME */
-    insecureApiKey: ,                                   /* FIXME */
-    welcomeMessage: ,                                   /* FIXME */
+    password: password.result,
+    stackPet: petName.id,
+    ownerPetName: ownerPetName,
+    apiKey: config.requireSecret("apiKey"),
+    insecureApiKey: pulumi.unsecret(config.requireSecret("apiKey")),
+    welcomeMessage: welcomeMessage,
     // TODO: 10. Set a stack README for your stack
-    readme: ,                                           /* FIXME */
+    readme: readFileSync("./Pulumi.README.md").toString(),
   };
 };
